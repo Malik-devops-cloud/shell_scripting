b947d09133c312c853d2529948e21e1f6f09f1e8
Malik <info@joindevops.com>
2026-03-04 19:54:37 +0530

    updated names

---
+++ 1.variables.sh
@@ -0,0 +1,9 @@
+PERSON1#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "PERSON1:: welcome to alaska PERSON2"
+echo "PERSON2:: Thankyou PERSON1,Finally not zoom webinar"
+echo "PERSON1:: Can we have a deal PERSON2?"
+echo "PERSON2:: If you ukrain to join russia"
