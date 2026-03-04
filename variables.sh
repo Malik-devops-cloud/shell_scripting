4dec589d3664a0ea59b8cfc249341cb58fb005a4
Malik <info@joindevops.com>
2026-03-04 16:14:58 +0530

    changes are done

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=Malik
+person2=Baazil
+
+echo "$person1::How are you $person2?"
+echo "$person2::Hi $person1, I am fine.what about you?"
+echo "$person1::yeah good $person2.Let's have cup of tea"
+echo "$person2::sure $person1"
