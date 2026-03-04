acea3676bba924e6cea4863f37618cf5a77747b6
Malik <info@joindevops.com>
2026-03-04 22:17:19 +0530

    updated names

--- 5.variables.sh
+++ 5.variables.sh
@@ -4,3 +4,8 @@ echo "all variables passed to the scripts: $@"
 echo "all variables passed to the scripts: $*"
 echo "script name: $0"
 echo "current working directory: $PWD"
+echo "who si running this directory: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of this script: $$"
+sleep 50&
+echo "PID of the last command in background is : $!"
