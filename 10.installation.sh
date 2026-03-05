a7bad28bce68312cdc75e83c22c0e97bc186cd5f
Malik <info@joindevops.com>
2026-03-05 15:37:45 +0530

    updated names

--- 10.installation.sh
+++ 10.installation.sh
@@ -2,13 +2,13 @@
 
 USERID=$(id -u)
 
-if [ $USERID -nq 0 ]; then
+if [ $USERID -ne 0 ]; then
    echo "error::please run this script with root previlage"
 fi   
 
 dnf install mysql -y
 
-if [ $? -nq 0 ]; then
+if [ $? -ne 0 ]; then
    echo "error::installing mysql is failure"
 else
    echo "installing mysql is success"
