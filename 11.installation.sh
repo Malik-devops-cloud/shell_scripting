253bfdc1ac9adab2696ec86fe64a10baa6991942
Malik <info@joindevops.com>
2026-03-05 16:02:40 +0530

    updated names

---
+++ 11.installation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+   echo "error::please run this script with root previlage"
+   exit 1
+fi   
+
+VALIDATE() {if [ $1 -ne 0 ]; then
+   echo "error::installing $2 is failure"
+   exit 1
+else
+   echo "installing $2 is success"
+fi   
+}
+
+dnf install mysql -y
+VALIDATE $? "mysql"
+
+dnf install nginx -y
+VALIDATE $? "nginx"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "mongodb-mongosh" 
