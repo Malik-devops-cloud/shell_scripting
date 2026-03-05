7235c37aa0499843136078d83055c392072244a5
Malik <info@joindevops.com>
2026-03-05 17:41:57 +0530

    updated names

--- 14-logfile.sh
+++ 14-logfile.sh
@@ -10,7 +10,7 @@ N="\e[37m"
 
 LOGS_FOLDER=/var/log/shell-script
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOGS_FILE=($LOGS_FOLDER/$SCRIPT_NAME.log)
+LOGS_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
 
 mkdir -p $LOGS_FILE
 echo "script executing at:$(date)"
